778988800aacc0141aa57590f648c48e50add6ef
Dean <deank242@gmail.com>
2022-05-20 13:43:00 -0400

    errorfix

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 //create connection to our database
 let sequelize;
 if (process.env.JAWSDB_URL) {
-    sequelize = new sequelize(process.env.JAWSDB_URL;)
+    sequelize = new sequelize(process.env.JAWSDB_URL)
 } else {
     sequelize = new sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
         host: 'localhost',
